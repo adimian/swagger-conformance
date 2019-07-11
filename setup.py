a3cc5b2ece44ab653908ba2380c56b0bbde999b3
Eric Gazoni <eric@adimian.com>
2019-07-11 11:55:36 +0200

    put the requirements where they make sense

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup(setup_requires=["pbr>=1.9", "setuptools>=17.1"], pbr=True)
+setup(
+    setup_requires=[
+        "pbr>=1.9",
+        "setuptools>=38.6.0",
+        "twine>=1.11.0",
+        "wheel>=0.31.0",
+    ],
+    pbr=True,
+)
